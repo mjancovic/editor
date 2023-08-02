e90b925eaffcdf9a1da0e24dd2f6c3ae08a56379
Milos Jancovic <mjancovic@qualityunit.com>
2023-08-02 13:27:35 +0200

    feat: Create simple switchable editor

---
+++ src/lib/Buttons.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { createEventDispatcher } from 'svelte';
+
+    const dispatch = createEventDispatcher();
+
+    function onHtml() {
+        dispatch('html', {});
+    }
+    function onPlain() {
+        dispatch('plain', {});
+    }
+</script>
+
+<button class="HTMLEditor" on:click={onHtml}>HTML</button>
+<button class="PlainEditor" on:click={onPlain}>Plain</button>
---
+++ src/lib/CKEditor.svelte
@@ -0,0 +1,19 @@
+<script>
+    import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+
+    export let value = '';
+
+    ClassicEditor
+        .create(value)
+        .then(newEditor => {
+            document.querySelector('#ckeditor').appendChild(newEditor.ui.element);
+            newEditor.model.document.on('change:data', () => {
+                value = newEditor.getData();
+            })
+        })
+        .catch(err => {
+            console.error(err.stack);
+        });
+</script>
+
+<div id="ckeditor"/>
