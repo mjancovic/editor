6af387023bba30c5bf9d36daa89eae5978d93335
Milos Jancovic <mjancovic@qualityunit.com>
2023-08-24 10:57:33 +0200

    feat: Add translations support

--- src/lib/CKEditor5Document.svelte
+++ src/lib/CKEditor5Document.svelte
@@ -3,12 +3,14 @@
 <script>
     import DecoupledEditor from '@ckeditor/ckeditor5-build-decoupled-document';
     import ImageUploadAdapter from './CKEditor5ImageUploadAdapter.ts';
+    import './CKEditor5Translations.ts';
 
     export let insertImageServiceUrl = '';
     export let getImageServiceUrl = '';
     export let items = ['bold', 'italic', 'strikethrough', 'underline', 'fontColor', 'fontBackgroundColor', 'fontFamily', 'fontSize', '|', 'numberedList', 'bulletedList', '|', 'outdent', 'indent', 'blockQuote', '|', 'link', '|', 'uploadImage', 'insertTable', '|', 'undo', 'redo'];
     export let value = '';
     export let placeHolder = '';
+    export let lang = 'en';
     export let change = (evt, data) => {
     };
     export let ready = (evt, data) => {
@@ -27,7 +29,10 @@
         .create(value, {
             placeholder: placeHolder,
             toolbar: items,
-            extraPlugins: [ImageUploadAdapterPlugin]
+            extraPlugins: [ImageUploadAdapterPlugin],
+            language: {
+                ui: lang
+            }
         })
         .then(newEditor => {
             editor = newEditor;
