131e2f103f4ecc8ceeccda00140bdf6441625902
Milos Jancovic <mjancovic@qualityunit.com>
2023-11-16 13:39:19 +0100

    build: 0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,26 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import { createRequire } from 'node:module';
-const require = createRequire( import.meta.url );
+import {resolve} from 'path'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
+import {createRequire} from 'node:module';
 import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5';
+
+const require = createRequire(import.meta.url);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svelte({
-      compilerOptions: {
-        customElement: true
-      },
-    }),
-    ckeditor5( { theme: require.resolve( '@ckeditor/ckeditor5-theme-lark' ) } )
-  ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/main.ts'),
+            name: 'Editor',
+            fileName: 'editor'
+        }
+    },
+    plugins: [
+        svelte({
+            compilerOptions: {
+                customElement: true
+            },
+        }),
+        ckeditor5({theme: require.resolve('@ckeditor/ckeditor5-theme-lark')})
+    ],
 })
